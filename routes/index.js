1bd2a16111d14fbec545d38b2569b46f1bf22774
SupRobin <flavio.m.duran@gmail.com>
2025-07-24 22:14:06 -0400

    updates

--- routes/index.js
+++ routes/index.js
@@ -17,13 +17,6 @@ router.post('/quiz/submit' , function(req, res) {
     res.send("Submitting...");
 })
 
-
-router.get('/quiz/setup', (req, res) => {
-    const quiz = readQuizDB();
-    res.render('setup', { totalQuestions: quiz.question.length });
-});
-
-
 function readQuizDB() {
     let data = fs.readFileSync(postDBFileName, "utf-8");
     return JSON.parse(data);
