3b8f0d0a2595077d3bc189bd5a48a9edec844d61
Renzo <renzo.salazarcueva@gmail.com>
2025-07-26 20:37:30 -0400

    updated layouts of main page and quiz

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,8 @@ const router = express.Router();
 const postDBFileName = "./model/questions.json";
 const { getQuestions } = require('../model/quizQuestions');
 
-
-router.get('/', function(req, res, next) {
-  res.render('./main/quiz-setup');
-});
-
-router.get('/quiz', function(req, res, next) {
-  let quiz = readQuizDB();
-  res.render('./main/quiz', {quiz: quiz});
+router.get('/', (req, res) => {
+    res.render('main/index');
 });
 
 router.post('/quiz/submit' , function(req, res) {
@@ -20,14 +14,7 @@ router.post('/quiz/submit' , function(req, res) {
 
 router.get('/quizgame', (req, res) => {
     const questions = getQuestions();
-    res.render('main/quizgame', {
-        questions
-    });
+    res.render('main/quizgame', { questions });
 });
-
-function readQuizDB() {
-    let data = fs.readFileSync(postDBFileName, "utf-8");
-    return JSON.parse(data);
-}
 
 module.exports = router;
