b268d522ee26478d31f7a85e3926bd539156cf9b
Renzo <renzo.salazarcueva@gmail.com>
2025-07-29 20:54:40 -0400

    user sign up

--- routes/index.js
+++ routes/index.js
@@ -17,51 +17,31 @@ router.get('/signup', (req, res) => {
     res.render('main/signup');
 });
 
+router.get('/signin', (req, res) => {
+    res.render('main/signin');
+});
+
 router.post('/signup/submit', async (req, res, next) => {
+    const { name, email, password } = req.body;
+    console.log('[SIGNUP] incoming:', { name, email });
     try {
-        const { username, email, password } = req.body;
-        if (!username || !email || !password) {
-            return res
-                .status(400)
-                .render('main/signup', {
-                    error: 'All fields are required.',
-                    formData: { username, email }
-                });
-        }
-
-        const usersCol = getCollection('users');
-        const existing = await usersCol.findOne({ email });
-        if (existing) {
-            return res
-                .status(409)
-                .render('main/signup', {
-                    error: 'That email is already in use.',
-                    formData: { username, email }
-                });
-        }
-
+        const users = getCollection('users');
         const hashedPassword = await bcrypt.hash(password, 10);
-
-        const result = await usersCol.insertOne({
-            username,
+        const { insertedId } = await users.insertOne({
+            name,
             email,
-            password: hashedPassword
+            password: hashedPassword,
         });
-        console.log('New user _id=', result.insertedId);
+        console.log(`[SIGNUP] user created with _id=${insertedId}`);
 
+        //redirect to signin page
         return res.redirect('/signin');
     } catch (err) {
-        if (err.code === 11000) {
-            return res
-                .status(409)
-                .render('main/signup', {
-                    error: 'That email is already registered.',
-                    formData: { username, email }
-                });
-        }
-        next(err);
+        console.error('[SIGNUP] error:', err);
+        return next(err);
     }
 });
+
 router.post('/quiz/submit' , function(req, res) {
     res.send("Submitting...");
 })
